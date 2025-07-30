0cfe9dde1a127e50e29fc4fe7a4e98aaea2c1fe4
LiberSoft <info@libersoft.org>
2025-07-30 17:08:16 +0200

    Fix commit script

--- commit.sh
+++ commit.sh
@@ -28,7 +28,7 @@ if [ ! -d "./.git/" ]; then
 else
  git remote set-url origin https://$USER:$PASS@github.com/$USER/$REPO
 fi
-bun i -g prettier prettier-plugin-svelte
+#bun i -g prettier prettier-plugin-svelte
 #prettier --plugin 'prettier-plugin-svelte' --write "src/**/*.{js,ts,css,html,svelte}"
 git config user.name "$NAME"
 git config user.email "$EMAIL"
