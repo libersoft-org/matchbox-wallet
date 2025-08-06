68e195cc3725ed9ec8b4b2ca6d8c05005813fe21
LiberSoft <info@libersoft.org>
2025-08-06 12:47:31 +0200

    Remove prettier

--- clean.sh
+++ clean.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo "Cleaning temporary QML formatting files..."
-find . -name "*-formatting-tmp.qml~" -type f -delete
-REMOVED=$(find . -name "*-formatting-tmp.qml~" -type f | wc -l)
-echo "All temporary files cleaned!"
+rm -rf ./build/
