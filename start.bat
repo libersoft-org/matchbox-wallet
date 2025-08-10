f4c522c6caac8fbd6ced9faa68ba61a42770fb0d
LiberSoft <info@libersoft.org>
2025-08-10 17:25:56 +0200

    Remove unused Windows scripts

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if exist "build\windows\wallet.exe" (
- build\windows\wallet.exe
-) else (
- echo Error: Application not found. Please build the project first by running:
- echo build.bat
- exit /b 1
-)
